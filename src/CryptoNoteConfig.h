5c20b45b2607d2ebe501b4d69c9987cebf60878e
Sebastian Newton <20719131+qwertyatom100@users.noreply.github.com>
2018-03-14 13:27:37 +1100

    Update CryptoNoteConfig.h
    It will be valid now.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "jimcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101960cf73a53dac11529d1c685b2c9e986ad5e39d8aeea23272496bfe07fec7e06";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
